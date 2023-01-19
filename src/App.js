20723c47a0c8e2a25386e7a8983ecd0232f5991e
FrenzyPet <manpatbro@gmail.com>
2023-01-19 20:56:43 +0300

    базовая верстка

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app-wrapper'>
+      <header className='main-header'>
+        <img src="https://spng.pngfind.com/pngs/s/4-48076_visual-website-optimizer-visual-website-optimizer-logo-hd.png" width="100" height="100"></img>
       </header>
+      <nav className='navigation'>
+        <ul>
+          <li>
+            <a>Profile</a>
+          </li>
+          <li>
+            <a>Messages</a>
+          </li>
+          <li>
+            <a>News</a>
+          </li>
+          <li>
+            <a>Music</a>
+          </li>
+          <li>
+            <a>Settings</a>
+          </li>
+        </ul>
+      </nav>
+      <section className='content'>
+        <img className='image-profile' src='https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg'></img>
+      </section>
     </div>
   );
 }
