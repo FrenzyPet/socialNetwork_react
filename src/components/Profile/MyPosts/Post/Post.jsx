8a0803faf10599c5a3ed4f4798bcbc6d5b39e110
FR3NZY <manpatbro@gmail.com>
2023-02-18 00:56:39 +0300

    some little fix

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,7 +4,7 @@ const Post = (props) => {
   return (
     <li className={classes.item}>
       <div className={classes.item__avatarWrapper}>
-        <img className={classes.item__avatarImage} src='https://bipbap.ru/wp-content/uploads/2022/11/1652235714_41-kartinkin-net-p-prikolnie-kartinki-dlya-stima-44.jpg'></img>
+        <img className={classes.item__avatarImage} src='https://bipbap.ru/wp-content/uploads/2022/11/1652235714_41-kartinkin-net-p-prikolnie-kartinki-dlya-stima-44.jpg' alt='img'></img>
       </div>
       <p className={classes.item__text}>{props.message}</p>
       <p className={classes.item__likes}>Likes: {props.likesCount}</p>
